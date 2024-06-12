21bac1977bfd3b67bf3840c5941e481ebfc505b7
mstrintzis <m.strintzis@acg.edu>
2024-06-12 16:28:54 +0300

    Dark mode added.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,60 @@
+let isDarkMode = false ;
+
 document.addEventListener('DOMContentLoaded' ,(event) =>{
 
     //Dark mode button
     const darkbutton = document.querySelector("#darkmode") ;
-    const lightbutton = document.querySelector("#lightmode") ;
+    let checkMode = false ;
 
-    //Dark mode button
     darkbutton.addEventListener('click',() => {
-       darkModeEnable() ;
-       console.log("Dark Mode.") ;
-    }) ;
+        if(checkMode){
+            lightModeEnable();
+            console.log("Light Mode") ;
+            isDarkMode = false ;
+        }
+        else{
+            darkModeEnable();
+            console.log("Dark Mode") ;
+            isDarkMode = true ;
+        }
+        checkMode = !checkMode ;
+    }
+    );
 
-     //Light mode button
-    lightbutton.addEventListener('click' ,() =>{
-        lightModeEnable();
-        console.log("Light Mode.") ;
-    });
 });
 
 
 function darkModeEnable (){
     const darkmode = document.querySelector("body") ;
-    darkmode.style.backgroundColor= "red" ;
-}
+    const button = document.querySelector("#darkmode") ;
+
+    darkmode.style.backgroundColor= "#2D2E40" ;
+    darkmode.style.color= "#FCF3F6" ;
+    
+    button.innerHTML = "Light Mode" ;
+    button.style.backgroundColor = "#ffff" ;
+    button.style.color = "#000000" ;
+}   
 
 function lightModeEnable(){
     const lightmode = document.querySelector("body") ;
-    lightmode.style.backgroundColor = "green" ;
+    const button = document.querySelector("#darkmode") ;
+
+    lightmode.style.backgroundColor = "#ffff" ;
+    lightmode.style.color = "#000000" ;
+
+    button.innerHTML = "Dark Mode" ;
+    button.style.backgroundColor = "#000000" ;
+    button.style.color = "#ffff";
+
+}
+
+//Check the Dark mode settings to convert the next page into dark mode if it is
+function checkDarkMode(){
+    if (isDarkMode){
+        console.log("It's Dark Mode") ;
+    }
+    else {
+        console.log("It's Light Mode") ;
+    }
 }
