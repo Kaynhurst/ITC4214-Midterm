f2c2a6fad0ef2626a1d073a264ba96da95920aa6
mstrintzis <m.strintzis@acg.edu>
2024-06-12 21:54:04 +0300

    Content table filled. Added sorting button-No functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,4 @@
-let isDarkMode = false ;
-
+/*Dark Mode */    {
 document.addEventListener('DOMContentLoaded' ,(event) =>{
 
     //Dark mode button
@@ -24,11 +23,13 @@ document.addEventListener('DOMContentLoaded' ,(event) =>{
 
 });
 
-
+ 
 function darkModeEnable (){
+
     const darkmode = document.querySelector("body") ;
     const button = document.querySelector("#darkmode") ;
     const table = document.querySelector("#contents") ;
+    const menu = document.querySelector("#menu") ;
 
     darkmode.style.backgroundColor= "#2D2E40" ;
     darkmode.style.color= "#FCF3F6" ;
@@ -36,13 +37,22 @@ function darkModeEnable (){
     button.innerHTML = "Light Mode" ;
     button.style.backgroundColor = "#FFFF" ;
     button.style.color = "#000000" ;
-    table.style.color = "#FFFFFF" ;
+    
+    //Check if there is a table on the page.
+    if (table != null){
+        table.style.color = "#FFFFFF" ;
+    }
+
+    menu.style.backgroundColor= "#2D2E40" ;
 }   
 
+
 function lightModeEnable(){
+
     const lightmode = document.querySelector("body") ;
     const button = document.querySelector("#darkmode") ;
     const table = document.querySelector("#contents") ;
+    const menu = document.querySelector("#menu") ;
 
     lightmode.style.backgroundColor = "#FFFF" ;
     lightmode.style.color = "#000000" ;
@@ -50,16 +60,30 @@ function lightModeEnable(){
     button.innerHTML = "Dark Mode" ;
     button.style.backgroundColor = "#000000" ;
     button.style.color = "#FFFF";
-    table.style.color = "#000000" ;
+
+    //Check if there is a table on the page.
+    if (table != null){
+        table.style.color = "#000000" ;
+    }
+   
+    menu.style.backgroundColor = "#FFFF" ;
 
 }
 
 //Check the Dark mode settings to convert the next page into dark mode if it is
 function checkDarkMode(){
+    
     if (isDarkMode){
         console.log("It's Dark Mode") ;
     }
     else {
         console.log("It's Light Mode") ;
     }
+}
+}
+
+
+
+/*Sorting System */{
+    
 }
